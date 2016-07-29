40655e305a906b0909d3a9197b4fa334ad7aed5f
chowza <terence.chow.1986@gmail.com>
2016-07-29 12:17:48 +0100

    add estimate for nth prime so we can use the current sieve

--- PrimeNumberMultiplicationTable.js
+++ PrimeNumberMultiplicationTable.js
@@ -5,16 +5,25 @@ function PrimeNumberMultiplicationTable(n){
 }
 
 function getPrimes(n){
-  var list = []
 
-  for (var i =2; i <= n; i++){
+
+  var estimateOfNthPrime
+  if (n >= 6) {
+    estimateOfNthPrime = n * Math.log(n) + n * Math.log(Math.log(n))
+  } else {
+    //If n < 6 we know that the 5th highest prime is 11
+    estimateOfNthPrime = 11
+  }
+
+  var list = []
+  for (var i =2; i <= estimateOfNthPrime; i++){
     list.push(i)
   }
 
-  for (var i=2; i < Math.sqrt(n); i++) {
+  for (var i=2; i < Math.sqrt(estimateOfNthPrime); i++) {
     if (list[i]){
       var i2 = Math.pow(i,2)
-      for (var k=0; i2 + k * i <= n; k++ ){
+      for (var k=0; i2 + k * i <= estimateOfNthPrime; k++ ){
         var j = i2 + k * i
         // - 2 because our list starts at 2 not 0.
         list[j - 2] = false
